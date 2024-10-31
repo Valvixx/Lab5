a320aaabccad034bdea280c2f25eccf85ba7ff9c
Valvixx <valvixx04@gmail.com>
2024-10-31 23:49:20 +0300

    task2

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int n = 10, ** G = NULL, size = 0, *degree = NULL, *loop = NULL;
+	int n = 10, ** G = NULL, size = 0, * degree = NULL, * loop = NULL;
 	printf("Введите количество вершин:");
 	scanf("%d", &n);
 	printf("\n");
@@ -29,7 +29,7 @@ int main()
 
 
 	for (int i = 0; i < n; i++)
-		for (int j = i; j < n; j++) 
+		for (int j = i; j < n; j++)
 		{
 			G[i][j] = rand() % 2;
 			G[j][i] = G[i][j];
@@ -37,8 +37,8 @@ int main()
 
 			if (i == j)
 			{
-				degree[i] += 2*G[i][j];
-				loop[i] += 2*G[i][j];
+				degree[i] += 2 * G[i][j];
+				loop[i] += 2 * G[i][j];
 			}
 			else
 			{
@@ -46,9 +46,9 @@ int main()
 			}
 		}
 
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++) 
+		for (int j = 0; j < n; j++)
 		{
 			printf("%d ", G[i][j]);
 		}
@@ -69,3 +69,68 @@ int main()
 	}
 
 
+	// ---Task 2---
+
+	int m, ** IncMatrix = NULL;
+
+	printf("Введите количество вершин: ");
+	scanf("%d", &n);
+
+	printf("Введите количество рёбер: ");
+	scanf("%d", &m);
+
+	// Выделение памяти для массива степеней и петель
+	degree = (int*)calloc(n, sizeof(int));
+	loop = (int*)calloc(n, sizeof(int));
+
+	// Выделение памяти для матрицы инцидентности
+	IncMatrix = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++)
+		IncMatrix[i] = (int*)calloc(m, sizeof(int));
+
+	// Заполнение матрицы инцидентности
+	for (int j = 0; j < m; j++) {
+		int v1, v2;
+		printf("Введите вершины для ребра %d (через пробел): ", j + 1);
+		scanf("%d %d", &v1, &v2);
+
+		if (v1 == v2) {
+			IncMatrix[v1][j] = 2; // Петля
+			degree[v1] += 2;
+			loop[v1] += 2;
+		}
+		else {
+			IncMatrix[v1][j] = 1;
+			IncMatrix[v2][j] = 1;
+			degree[v1]++;
+			degree[v2]++;
+		}
+	}
+
+	// Вывод матрицы инцидентности
+	printf("\nМатрица инцидентности:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d ", IncMatrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	for (int i = 0; i < n; i++) {
+		if (degree[i] - loop[i] == 0)
+			printf("Вершина %d - Изолированная\n", i);
+		if (degree[i] - loop[i] == 1)
+			printf("Вершина %d - Концевая\n", i);
+		if (degree[i] == n - 1)
+			printf("Вершина %d - Доминирующая\n", i);
+	}
+
+	for (int i = 0; i < n; i++)
+		free(IncMatrix[i]);
+	free(IncMatrix);
+	free(degree);
+	free(loop);
+
+	return 0;
+}
